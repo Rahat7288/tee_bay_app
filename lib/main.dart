789e05b7cc850da5e1877cdf9d8f906da9f4dcd5
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-03 15:41:02 +0600

    get storage for local storage service added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'core/local_storage/storage_service.dart';
+
+void main() async {
+  await StorageService.init();
+
   runApp(const MyApp());
 }
 
