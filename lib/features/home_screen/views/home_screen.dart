e7f98b73f099c91a2e29ecf9f73dfdc6cce34ded
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-04 00:43:48 +0600

    home screen product list and list cart design  added, drawer widget added

--- lib/features/home_screen/views/home_screen.dart
+++ lib/features/home_screen/views/home_screen.dart
@@ -1,50 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:tee_bay_app/core/app_theme/app_colors.dart';
+import 'package:tee_bay_app/core/text_style/text_style.dart';
 
-import '../../../resources/reusable_widgets/text_inputs/custom_password_field.dart';
-import '../../../resources/reusable_widgets/text_inputs/custom_text_field.dart';
+import '../../../resources/drawer/app_drawer.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-class _HomeScreenState extends State<HomeScreen> {
-  final _inputController = TextEditingController();
-  final _passWordController = TextEditingController();
-  bool isObscure = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        customInputField(
-          controller: _inputController,
-          type: TextInputType.text,
-          labelText: 'User Name',
-          hintText: 'User Name',
-          onChanged: (String value) {},
+        key: _scaffoldKey,
+        drawer: Drawer(
+          child: appDrawer(context: context),
         ),
-        const SizedBox(
-          height: 10,
+        appBar: AppBar(
+          title: Text(
+            'ALL PRODUCTS',
+            style: TStyle.subTitle(color: AppColor.titleTextColor),
+          ),
         ),
-        customPasswordField(
-            controller: _passWordController,
-            labelText: 'Password',
-            hintText: 'Password',
-            isObscure: isObscure,
-            suffixIcon: isObscure ? Icons.visibility_off : Icons.visibility,
-            onChanged: (String value) {},
-            textInputAction: TextInputAction.done,
-            press: () {
-              setState(() {
-                isObscure = !isObscure;
-              });
-            }),
-      ],
-    ));
+        body: ListView.separated(
+          itemCount: 25,
+          separatorBuilder: (BuildContext context, int index) => const SizedBox(
+            height: 10,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return _buildItemCart();
+          },
+        ));
+  }
+
+  Widget _buildItemCart() {
+    String title = 'Title';
+    String subTitle = 'Category';
+    String price = 'Price';
+    String content =
+        ' content content content content content content content ';
+    String postedDate = 'postedDate';
+    String views = 'Views';
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 14),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: AppColor.borderColor,
+          style: BorderStyle.solid,
+        ),
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TStyle.title(),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '$subTitle :',
+                    style: TStyle.subTitle(
+                      color: AppColor.subTitleColor,
+                    ),
+                  ),
+                  Text(subTitle),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '$price :',
+                    style: TStyle.subTitle(
+                      color: AppColor.subTitleColor,
+                    ),
+                  ),
+                  Text(price),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                content,
+                maxLines: 3,
+                textAlign: TextAlign.start,
+                overflow: TextOverflow.ellipsis,
+                style: TStyle.contentText(
+                  color: AppColor.contentTextColor,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      postedDate,
+                      style: TStyle.subTitle(color: AppColor.subTitleColor),
+                    ),
+                  ),
+                  Text(
+                    views,
+                    style: TStyle.subTitle(color: AppColor.subTitleColor),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(
+            width: 3,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.delete,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/resources/drawer/app_drawer.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:tee_bay_app/core/app_theme/app_colors.dart';
+
+Widget appDrawer({required BuildContext context}) => Container(
+      color: AppColor.backGroundColor,
+      child: ListView(
+        children: [],
+      ),
+    );
