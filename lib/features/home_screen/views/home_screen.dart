025c11a57bbcc3fbd1fbfa8ba3f890f826bbaa8c
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-04 02:26:02 +0600

    product details page added

--- lib/features/home_screen/views/home_screen.dart
+++ lib/features/home_screen/views/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tee_bay_app/core/app_theme/app_colors.dart';
 import 'package:tee_bay_app/core/text_style/text_style.dart';
+import 'package:tee_bay_app/features/home_screen/views/widgets/all_product_body.dart';
 
 import '../../../resources/drawer/app_drawer.dart';
 import 'widgets/my_product_body.dart';
@@ -9,6 +10,8 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool myProduct = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +22,17 @@ class HomeScreen extends StatelessWidget {
       ),
       appBar: AppBar(
         title: Text(
-          'ALL PRODUCTS',
+          myProduct ? 'MY PRODUCT' : 'ALL PRODUCTS',
           style: TStyle.subTitle(color: AppColor.titleTextColor),
         ),
       ),
-      body: myProductBody(),
+      body: myProduct ? myProductBody() : allProductBody(context: context),
+      floatingActionButton: myProduct
+          ? FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            )
+          : null,
     );
   }
 }
