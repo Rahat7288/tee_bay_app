a15bb0aa5852b3a469b0fc339119f0ff828d564a
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-04 16:43:57 +0600

    get api service function added

--- lib/core/networks/network_api_services.dart
+++ lib/core/networks/network_api_services.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:tee_bay_app/core/networks/base_api_services.dart';
 
@@ -23,15 +24,22 @@ class NeTworkApiServices extends BasApiServices {
   }
 
   @override
-  Future deleteApi(url, BuildContext context) {
-    // TODO: implement deleteApi
-    throw UnimplementedError();
-  }
+  Future<dynamic> getApi(String url, data, BuildContext context) async {
+    if (kDebugMode) {
+      print(url);
+      print(data);
+    }
 
-  @override
-  Future getApi(String url, data, BuildContext context) {
-    // TODO: implement getApi
-    throw UnimplementedError();
+    Dio dio = Dio(await getBaseOptions(query: data));
+
+    dynamic responseJson;
+
+    try {
+      Response response = await dio.get(url, data: data);
+      responseJson = returnResponse(response, context);
+    } catch (e) {}
+
+    return responseJson;
   }
 
   @override
@@ -41,8 +49,35 @@ class NeTworkApiServices extends BasApiServices {
   }
 
   @override
+  Future deleteApi(url, BuildContext context) {
+    // TODO: implement deleteApi
+    throw UnimplementedError();
+  }
+
+  @override
   Future updateApi(payload, url, BuildContext context) {
     // TODO: implement updateApi
     throw UnimplementedError();
+  }
+
+  dynamic returnResponse(Response response, context) async {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = response.data;
+        return responseJson;
+      case 201:
+        dynamic responseJson = response.data;
+        return responseJson;
+
+      case 400:
+        dynamic responseJson = response.data;
+        return responseJson;
+
+      case 500:
+        dynamic responseJson = response.data;
+        return responseJson;
+
+      default:
+    }
   }
 }
