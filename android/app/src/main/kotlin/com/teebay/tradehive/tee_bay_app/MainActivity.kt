f8eecdeb20abe18c6a65eaf5d2eb94351e081596
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-05 01:03:44 +0600

    loacl authentication package added for biometric login system

--- android/app/src/main/kotlin/com/teebay/tradehive/tee_bay_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/teebay/tradehive/tee_bay_app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.teebay.tradehive.tee_bay_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
