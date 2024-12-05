d3da3eaf3e0ef259f37d2caf0d4122573e54de71
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-06 00:57:45 +0600

    theme cubit removed from main (tast case)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:tee_bay_app/core/app_theme/cubits/theme_cubit.dart';
-import 'package:tee_bay_app/core/app_theme/cubits/theme_state.dart';
-import 'package:tee_bay_app/core/app_theme/repository/theme_repository.dart';
 
 import 'core/data/local_storage/storage_service.dart';
 import 'features/auth/view_models/login_cubit/login_cubit.dart';
@@ -17,17 +13,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageService.init();
 
-  final themeRepository = ThemeRepository(GetStorage());
-
-  runApp(MyApp(
-    themeRepository: themeRepository,
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final ThemeRepository themeRepository;
-
-  MyApp({super.key, required this.themeRepository});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -35,23 +25,19 @@ class MyApp extends StatelessWidget {
     return MultiBlocListener(
         listeners: [
           BlocProvider(
-            create: (_) => ThemeCubit(themeRepository),
-          ),
-          BlocProvider(
               create: (context) => ProgressCubit(totalSteps: fromSteps.length)),
           BlocProvider(create: (context) => HomeScreenCubit()),
           BlocProvider(create: (context) => LoginCubit()),
           BlocProvider(create: (context) => SignupCubit()),
         ],
-        child: BlocBuilder<ThemeCubit, ThemeState>(
-          builder: (context, state) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Flutter Demo',
-              theme: state.themeData,
-              home: LogInScreen(),
-            );
-          },
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: LogInScreen(),
         ));
   }
 }
