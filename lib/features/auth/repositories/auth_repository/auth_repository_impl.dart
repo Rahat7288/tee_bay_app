31b3b72a7fccf1bd286791e9be2e6b9c5df88509
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-05 16:16:41 +0600

    signup function added to auth repository implementation

--- lib/features/auth/repositories/auth_repository/auth_repository_impl.dart
+++ lib/features/auth/repositories/auth_repository/auth_repository_impl.dart
@@ -3,6 +3,7 @@ import 'package:tee_bay_app/core/data/networks/network_api_services.dart';
 import 'package:tee_bay_app/resources/app_urls.dart';
 
 import '../../models/login_model/login_response_model.dart';
+import '../../models/signup_model/signup_model.dart';
 import 'auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
@@ -25,8 +26,14 @@ class AuthRepositoryImpl implements AuthRepository {
   }
 
   @override
-  Future signup({context, payload}) {
-    // TODO: implement signup
-    throw UnimplementedError();
+  Future<SignupModel> signup({context, payload}) async {
+    dynamic response =
+        await _apiService.postApi(payload, AppUrls.userRegister, context);
+
+    if (kDebugMode) {
+      print("signup response $response");
+    }
+
+    return SignupModel.fromJson(response);
   }
 }
