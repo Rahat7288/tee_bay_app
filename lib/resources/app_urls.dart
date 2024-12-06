875533fa65e357fa46a4a11e7c4807c618f42dae
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-06 19:28:47 +0600

    product categories model file added for state managemanet

--- lib/resources/app_urls.dart
+++ lib/resources/app_urls.dart
@@ -2,4 +2,7 @@ class AppUrls {
   static const String baseUrl = "http://localhost:8666/api";
   static const String signInUrl = "$baseUrl/users/login/";
   static const String userRegister = "$baseUrl/users/register/";
+
+  ///product related APIS============
+  static const String productCategories = "$baseUrl/products/categories/";
 }
