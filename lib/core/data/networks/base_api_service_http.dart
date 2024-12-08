54e6179faac06964a997ac125a02d4d88f1019e1
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-08 13:00:21 +0600

    api service class minor update with buildContext

--- lib/core/data/networks/base_api_service_http.dart
+++ lib/core/data/networks/base_api_service_http.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
 abstract class BaseNetworkService {
-  Future<dynamic> getRequest(String url, {Map<String, String>? headers});
+  Future<dynamic> getRequest(String url,
+      {Map<String, String>? headers, BuildContext? context});
   Future<dynamic> postRequest(String url,
-      {Map<String, String>? headers, Map<String, dynamic>? body});
+      {Map<String, String>? headers,
+      Map<String, dynamic>? body,
+      BuildContext? context});
   Future<dynamic> putRequest(String url,
-      {Map<String, String>? headers, Map<String, dynamic>? body});
-  Future<dynamic> deleteRequest(String url, {Map<String, String>? headers});
+      {Map<String, String>? headers,
+      Map<String, dynamic>? body,
+      BuildContext? context});
+  Future<dynamic> deleteRequest(String url,
+      {Map<String, String>? headers, BuildContext? context});
 }
