b9356aa99e41da60f22686709c67b7acdf61cae1
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-12-08 22:32:59 +0600

    post and update product repository implementation updated

--- lib/features/home_screen/domain/repositories/products_repository.dart
+++ lib/features/home_screen/domain/repositories/products_repository.dart
@@ -1,7 +1,7 @@
 class ProducRepository {
   Future<dynamic> getMyProduct({context, payload}) async {}
   Future<dynamic> getProducts({context, payload}) async {}
-  Future<dynamic> posProduct({context, payload}) async {}
+  Future<dynamic> postProduct({context, payload}) async {}
   Future<dynamic> updateProduct({context, payload}) async {}
   Future<dynamic> deleteProduct({context, payload}) async {}
 }
